fee4d9951644056ac4b4a65b4bf1656ac377788b
joelbay03 <90229527+joelbay03@users.noreply.github.com>
2022-10-31 10:39:27 -0700

    Add files via upload

--- Test_automateexcel.py
+++ Test_automateexcel.py
@@ -1,10 +1,18 @@
+
 from openpyxl import workbook, load_workbook
+import datetime
+
 
 wb=load_workbook('daily_overview(FKA 2hr).xlsx')
 
-wb.create_sheet("Test")
-print(wb.sheetnames)
+#current_date = dt.date.today()
+todays_date = str(datetime.datetime.now().strftime("%m-%d-%y"))
+
+wb.copy_worksheet(wb.active)
+
+#wb.title = todays_date
+
+#print(wb.title)
 
 wb.save('daily_overview(FKA 2hr).xlsx')
-#ws = wb.active
-#print(ws['C2'].value)
+
